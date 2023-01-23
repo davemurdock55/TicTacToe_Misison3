b79b74df76d7b2f12295cb0772bb811767898610
Mason Hunter <89164883+masonnh@users.noreply.github.com>
2023-01-23 15:25:43 -0700

    made supporting class with printBoard and determineWinner methods

--- TicTacToe_Misison3/Supporting.cs
+++ TicTacToe_Misison3/Supporting.cs
@@ -8,5 +8,72 @@ namespace TicTacToe_Misison3
 {
     class Supporting
     {
+        public void printBoard(char[,] arr)
+        {
+            // print out a pretty board, subing in arr values for the squares on the board
+            Console.WriteLine("   a     b     c  ");
+            Console.WriteLine("      |     |     ");
+            Console.WriteLine("1  " + arr[0, 0] + "  |  " + arr[1, 0] + "  |  " + arr[2, 0] + "  ");
+            Console.WriteLine(" _____|_____|_____");
+            Console.WriteLine("      |     |     ");
+            Console.WriteLine("2  " + arr[0, 1] + "  |  " + arr[1, 1] + "  |  " + arr[2, 1] + "  ");
+            Console.WriteLine(" _____|_____|_____");
+            Console.WriteLine("      |     |     ");
+            Console.WriteLine("3  " + arr[0, 2] + "  |  " + arr[1, 2] + "  |  " + arr[2, 2] + "  ");
+            Console.WriteLine("      |     |     ");
+        }
+
+        public int determineWinner(char[,] arr)
+        {
+            // winner variable, 0 means no winner, 1 means x winner, 2 means o winner
+            // default no winner
+            int winner = 0;
+
+            // stores if x or o wins, space if no win
+            char winningShape = ' ';
+
+            // check the top left to bottom right diagonal
+            if ((arr[0,0] == arr[1,1]) && (arr[0,0] == arr[2,2]))
+            {
+                winningShape = arr[0, 0];
+            }
+
+            // check the top right to bottom left diagonal
+            if ((arr[2, 0] == arr[1, 1]) && (arr[2, 0] == arr[0, 2]))
+            {
+                winningShape = arr[2, 0];
+            }
+
+            // loop through the rows and check if all values in row are equal
+            for (int i = 0; i < 3; i++)
+            {
+                if ((arr[0, i] == arr[1, i]) && (arr[0, i] == arr[2, i]))
+                {
+                    winningShape = arr[0, i];
+                }
+            }
+
+            // loop through the cols and check if all values in cols are equal
+            for (int i = 0; i < 3; i++)
+            {
+                if ((arr[i, 0] == arr[i, 1]) && (arr[i, 0] == arr[i, 2]))
+                {
+                    winningShape = arr[i, 0];
+                }
+            }
+
+            // check if x or o has won and set the return value
+            if (winningShape == 'x')
+            {
+                winner = 1;
+            }
+            else if (winningShape == 'o')
+            {
+                winner = 2;
+            }
+            
+            // 0 = no winner, 1 = x winner, 2 = o winner
+            return winner;
+        }
     }
 }
